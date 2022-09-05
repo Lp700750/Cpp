60a4e23510d4ba17f768b0cdcfbdf7347b7692c3
Lp700750 <104414865+Lp700750@users.noreply.github.com>
2022-09-05 16:50:29 +0800

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -1,10 +1,1 @@
-#define _CRT_SECURE_NO_WARNINGS 1\
-#include<iostream>
-#include"Vector.h"
-using namespace std;
-int main()
-{
-	Lp700::Vector<int> v1;
-
-	return 0;
-}
+#define _CRT_SECURE_NO_WARNINGS 1
