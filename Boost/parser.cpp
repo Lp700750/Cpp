f215c852d9ed2b4784cc92d881b2a3151c87a096
Lp700750 <2357543706@qq.com>
2022-12-21 20:51:27 +0800

    Boost搜索引擎代码

--- Boost/parser.cpp
+++ Boost/parser.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<boost/filesystem.hpp>
+#include"until.hpp"
 const std::string original="data/input/";
 const std::string result="data/raw_html/raw_txt";
-struct DocInfo
+typedef struct DocInfo
 {
 	std::string title;
 	std::string content;
@@ -13,10 +15,10 @@ struct DocInfo
 bool TransFunc1(const std::string& original,std::vector<std::string>* file);
 
 //第二步：将文件夹里面的文件进行解析
-bool AnanFunc(std::vector<std::string>* file,std::vector<docinfo_t>& docinfo);
+bool AnanFunc(std::vector<std::string>* file,std::vector<docinfo_t>* docinfo);
 
 //第三步：将解析好的文件保存在输出文件夹里面
-bool TransFunc2(std::vector<docinfo_t>* docinfo,const std::string& result);
+//bool TransFunc2(std::vector<docinfo_t>* docinfo,const std::string& result);
 int main()
 {
 	std::vector<std::string> file;
@@ -25,22 +27,55 @@ int main()
 		std::cerr<<"TransFunc1 error!"<<std::endl;
 		return 1;
 	}
-
-	if(!AnanFunc(&file,docinfo))
+	std::vector<docinfo_t> docinfo;
+	if(!AnanFunc(&file,&docinfo))
 	{
-		std::cerr<<"AnanFunc error!<<"std::endl;
+		std::cerr<<"AnanFunc error!"<<std::endl;
 		return 2;
 	}
 	
-	if(!TransFunc2(&docinfo,&result))
+	/*if(!TransFunc2(&docinfo,result))
 	{
 		std::cerr<<"TransFunc2 error"<<std::endl;	
 		return 3;
-	}
+	}*/
 
 	return 0;
 }
 bool TransFunc1(const std::string& original,std::vector<std::string>* file)
 {
-	
+	namespace fs=boost::filesystem;
+	fs::path original_path(original);
+	if(!fs::exists(original))
+	{
+		std::cerr<<"path not exits!"<<std::endl;
+		return false;
+	}
+	fs::recursive_directory_iterator finish;
+	for(fs::recursive_directory_iterator iterator(original_path);iterator!=finish;iterator++)
+	{
+		if(!fs::is_regular_file(*iterator))
+		{
+			continue;
+		}
+		if(iterator->path().extension()!=".html")
+		{
+			continue;
+		}
+		std::cout<<iterator->path().string()<<std::endl;
+		file->push_back(iterator->path().string());
+	}
+	return true;
+}
+bool AnanFunc(std::vector<std::string>* file,std::vector<docinfo_t>* docinfo)
+{
+	for(const std::string &_file:file)
+	{
+		const std::string result;
+	//1.读取指定文件夹里面的文件
+		if(!until::FileTool::ReadFile(file,&result))	
+	//2.将读取的文件解析title
+	//3.将读取的文件解析content
+	//4.将读取的文件解析url
+	}
 }
---
+++ Boost/until.hpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+namespace until
+{
+	class FileTool
+	{
+	public:
+		static bool ReadFile(const std::string &FilePath,std::string *OutContent)
+		{
+			
+			return true;	
+		}
+	}
+}
