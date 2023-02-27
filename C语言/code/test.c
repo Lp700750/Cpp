ae4bc052f4bef55b3cfc8a00fe06ddad681b6e7b
Lp700750 <2357543706@qq.com>
2023-02-27 16:56:54 +0800

    C注意点

---
+++ C语言/code/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1]
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	//char arr1[] = "hello world";
+	//char arr2[] = { 'h','e','l','l','o' };
+	//char arr3[] = { 'h','e','l','l','o' ,'\0'};
+
+	//int size1 = sizeof(arr1) / sizeof(arr1[0]);
+	//int size2 = sizeof(arr2) / sizeof(arr2[0]);
+	//int size3 = sizeof(arr3) / sizeof(arr3[0]);
+
+	//int size4 = strlen(arr1);
+	//int size5 = strlen(arr2);
+	//int size6 = strlen(arr3);
+
+	//printf("%d\n", size1);
+	//printf("%d\n", size2);
+	//printf("%d\n", size3);
+
+	//printf("%d\n", size4);
+	//printf("%d\n", size5);
+	//printf("%d\n", size6);
+
+	//printf("%s\n", arr1);
+	//printf("%s\n", arr2);
+	//printf("%s\n", arr3);
+
+	//printf("hello\n");
+	//printf("hello\r");
+
+#define NUM 100
+	typedef int INT;
+
+	INT a = 10;
+	printf("%d\n", NUM);
+	printf("%d\n", a);
+
+	return 0;
+}
